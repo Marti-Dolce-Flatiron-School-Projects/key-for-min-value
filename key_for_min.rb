27b71265a7458dfd3ee28ec8f45e1c54d602d4ac
Martinique <me@martidolce.com>
2020-11-14 17:07:53 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
-# prereqs: iterators, hashes, conditional logic
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+# # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(name_hash)
+  lowest_key = name_hash.reduce do |key, value|
+    key.last > value.last ? value : key
+  end
+  if name_hash == {}
+    lowest_key
+  else
+    lowest_key.first
+  end
 end
