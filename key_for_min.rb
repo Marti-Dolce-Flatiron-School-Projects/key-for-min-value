9e266749eba8b5d5b0d6b2415965c1d98771c903
Martinique <me@martidolce.com>
2020-11-14 19:20:42 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,24 @@
 # Course: Flatiron School 2020, November 9 - 20201, April 2021
 # Contact: me@martidolce.com | https://modis.martidolce.com
 # # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# Given a hash with numeric values,
+#
+# return the key for the smallest value
+#
 
+def key_for_min_value(my_hash)
+  #what is my value?
+  lowest_value = 500
+  lowest_key = nil
 
-def key_for_min_value(name_hash)
-  lowest_key = name_hash.reduce do |key, value|
-    key.last > value.last ? value : key
+  my_hash.collect do |key, value|
+    # what is my value?
+    if value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
   end
-  if name_hash == {}
-    lowest_key
-  else
-    lowest_key.first
-  end
+  lowest_key
 end
+
+# return key with smallest value
